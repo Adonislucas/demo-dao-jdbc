e7a3861afb19ef8a9a3eae37b91c17ee6ea12087
Adonis <adonai-lucai@hotmail.com>
2025-07-04 09:42:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department dp = new Department(1, "Books");
+        Seller seller = new Seller(1, "lucas", "adonai@hotmail.com", new Date(), 1000.0, dp);
 
-        System.out.println(dp);
+        System.out.println(seller);
 
 
 
